a84a0f808eb87efca7fd114fc2953089e5093e06
Steve Burket <burketsteve@gmail.com>
2021-04-27 12:20:32 -0500

    images on DOM

--- client.js
+++ client.js
@@ -1,1 +1,38 @@
 console.log('Here are all the available people:', people);
+
+// [x] step 0: add jquery to the project
+// [] step 0.5: randomly pick one person to be the guess
+//    store that globally, show on the DOM
+// [] step 1: loop over all the data
+//      -This is an image tag: <img src="https://github.com/booherbg.png?size=250" alt="Profile image of Blaine">
+// [x] step 2: append an image for each person (src=github.com/:username:.png)
+// [] step 3: make sure to store the person's name in a data attribute
+// [] step 4: in the click handler, compare the image clicked (name) to the game's answer
+// [] step 5: reset the game (call a function that repeats step 0.5)
+ 
+let userPics = [];
+
+function onReady() {
+    console.log('jQ');
+    for (let person of people) {
+        console.log(person.githubUsername); //when we get a value from an object: person.githubUsername
+        // make tags in divs to append images to DOM
+        // make variable to store images
+        let pic = `<img src="https://github.com/${person.githubUsername}.png?size=250"/>`;
+        console.log(pic);
+        userPics.push(pic); 
+
+    } // end of for loop
+
+    // loop userPics and create <di> with images
+    for (let pic of userPics) {
+        //grab the content main element
+        $(".content-main").append(`<div class="image-block">${pic}</div>`);
+
+
+    } // end for loop
+}
+
+
+
+$(document).ready(onReady);
